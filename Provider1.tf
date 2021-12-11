a1a418dfed31dd12d5ae21511ba86a3aabdc6414
gireesh01b <gireesh01b@gmail.com>
2021-12-12 03:18:18 +0530

    mainfile

---
+++ Provider1.tf
@@ -0,0 +1,5 @@
+provider "aws"{
+access_key=var.AWS_ACCESSKEY
+secret_key=var.AWS_SECRETKEY
+region="ap-south-1"
+}
---
+++ main.tf
@@ -0,0 +1,5 @@
+resource"aws_instance""Myterraform"{
+ami="ami-0ae12517fe595ce17"
+instance_type="t2.micro"
+
+}
